c973268d685cf43f6b959cda65468b2d1820f806
coderwei <916606569@qq.com>
2025-01-03 16:14:47 +0800

    feat: update canReachTargetExperience type

--- src/core.ts
+++ src/core.ts
@@ -25,10 +25,31 @@ export function canReachTargetExperience(
    staminaExp = 120,
    // 意外所得经验值
    extraStaminaExp = 0,
-   callback = (startTime: string, endTime: string, expDiff: number, totalExperience: number, days: number, staminaExp: number) => {}
-  }
- ) {
+   callback = (startTime: string, endTime: string, expDiff: number, totalExperience: number, days: number, staminaExp: number) => {
+    console.log('startTime: ', startTime)
+    console.log('endTime: ', endTime)
+    console.log('expDiff: ', expDiff)
+    console.log('totalExperience: ', totalExperience)
+    console.log('days: ', days)
+    console.log('staminaExp: ', staminaExp)
+   }
+  }: {
+    currentLevel: number;
+    targetLevel?: number;
+    currentExp: number;
+    startTime: string;
+    endTime: string;
+    recoveryPer5Min: number;
+    dailyFixedStamina: number;
+    extraDailyStamina: number;
+    extraDailyExp: number[];
+    extraStamina: number;
+    staminaExp: number;
+    extraStaminaExp: number;
+    callback: (startTime: string, endTime: string, expDiff: number, totalExperience: number, days: number, staminaExp: number) => void;
+  }) {
   const expDiff = calcExpDiff(currentLevel,currentExp, targetLevel)
+  console.log('expDiff: ', expDiff);
 
    // 计算时间跨度（分钟）
    const start = new Date(startTime).getTime()
@@ -43,19 +64,19 @@ export function canReachTargetExperience(
  
    // 计算每日体力
    const days = Math.ceil(timeDiffMinutes / (24 * 60))
-   console.log('days', days)
+   console.log('天数: ', days)
  
    const totalDailyStamina = days * (dailyFixedStamina + extraDailyStamina)
- 
+
    // 每日额外经验值
    const totalExtraDailyExp = days * extraDailyExp.reduce((sum, exp) => sum + exp, 0)
  
    // 总体力转换为经验值
    const totalStaminaExperience = (recoveredStamina + totalDailyStamina + extraStamina) * staminaExp
- 
    // 总经验值
    const totalExperience = totalStaminaExperience + totalExtraDailyExp + extraStaminaExp
- 
+   console.log('总体力: ', recoveredStamina + totalDailyStamina + extraStamina)
+   console.log('总经验值: ', totalExperience)
    // 判断是否达到目标经验值
    return callback(startTime, endTime, expDiff, totalExperience, days, staminaExp);
  }
